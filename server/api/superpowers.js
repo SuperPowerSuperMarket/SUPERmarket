aec396b8cfe5df9c3b61b362636300cfb6abe4ad
Sarah Laine <sjlaine624@gmail.com>
2018-02-26 17:07:29 -0500

    express routes for superpowers

---
+++ server/api/superpowers.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+const {Superpower} = require('../db/models');
+module.exports = router;
+
+router.get('/', (req, res, next) => {
+  Superpower.findAll()
+    .then(superpowers => res.json(superpowers))
+    .catch(next);
+});
+
+router.get('/:id', (req, res, next) => {
+  Superpower.findAll({
+    // include: [{model: Review}],
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(superpower => res.json(superpower))
+  .catch(next);
+});
+
+router.post('/', (req, res, next) => {
+  Superpower.create(req.body,
+    // {[{model: Review}]}
+  )
+  .then(superpower => res.json(superpower))
+  .catch(next);
+});
+
+router.put('/:id', (req, res, next) => {
+  Superpower.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(superpower => res.json(superpower))
+  .catch(next);
+});
+
+router.delete('/:id', (req, res, next) => {
+  Superpower.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(() => res.send('deleted superpower'))
+  .catch(next);
+});
