102adb98740a3b88c0d2b03200d4c151c5cfe356
Luis Rincon <rincon_92@hotmail.com>
2018-02-28 15:40:59 -0500

    began working on single superpower

--- client/components/SingleSuperpower.js
+++ client/components/SingleSuperpower.js
@@ -1,38 +1,40 @@
-// import React from 'react'
-// import { Card, Icon, Image } from 'semantic-ui-react'
-// import { connect } from 'react-redux';
-// import { withRouter } from 'react-router-dom'
+import React from 'react'
+import { Card, Icon, Image } from 'semantic-ui-react'
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom'
 
-// const mapStateToProps = (state) => {
-//     return {
-//       superpowers: state.superpowers
-//     }
-//   }
 
-// const SingleSuperpower = () => (
-//   <Card>
-//     <Image src='/assets/images/avatar/large/matthew.png' />
-//     <Card.Content>
-//       <Card.Header>
-//         Matthew
-//       </Card.Header>
-//       <Card.Meta>
-//         <span className='date'>
-//           Joined in 2015
-//         </span>
-//       </Card.Meta>
-//       <Card.Description>
-//         Matthew is a musician living in Nashville.
-//       </Card.Description>
-//     </Card.Content>
-//     <Card.Content extra>
-//       <a>
-//         <Icon name='user' />
-//         22 Friends
-//       </a>
-//     </Card.Content>
-//   </Card>
-// )
+const SingleSuperpower = (props) => {
+    const currentSuperpowerId = this.props.match.params.superpowerId;
+    console.log(currentSuperpowerId)
+    const singlePower = props.superpowers.find(superpower => superpower.id === currentSuperpowerId)
+    return (
+        <Card>
+        <Image src={singlePower.imageUrl} />
+        <Card.Content>
+        <Card.Header>
+        {singlePower.name}
+        </Card.Header>
+        <Card.Meta>
+        <span className='date'>
+        Joined in 2015
+        </span>
+        </Card.Meta>
+        <Card.Description>
+        {singlePower.description}
+        </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+        <a>
+        <Icon name='user' />
+        22 Friends
+        </a>
+        </Card.Content>
+        </Card>
+    )
+}
 
-// const SingleSuperpowerContainer = withRouter(connect(mapStateToProps))(SingleSuperpower)
-// export default SingleSuperpowerContainer;
+const mapStateToProps = state => ({superpowers: state.superpowers})
+
+const SingleSuperpowerContainer = withRouter(connect(mapStateToProps))(SingleSuperpower)
+export default SingleSuperpowerContainer;
--- client/components/all-superpowers.js
+++ client/components/all-superpowers.js
@@ -1,38 +1,9 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import { Grid, Card, Image } from 'semantic-ui-react'
+import { Grid, Image } from 'semantic-ui-react'
 import { withRouter, NavLink } from 'react-router-dom'
 
-const mapStateToProps = (state) => {
-  return {
-    superpowers: state.superpowers
-  }
-}
-
 const AllSuperpowers = (props) => {
-//   return (
-//     <div>
-//   {props.superpowers.superpowers.map((superpower) => {
-//     return (
-//       <Card key={superpower.id}>
-//         <Image src={superpower.imageUrl} />
-//         <Card.Content>
-//           <Card.Header>
-//             {superpower.name}
-//           </Card.Header>
-//           <Card.Meta>
-//             {/* {superpower.price} */}
-//           </Card.Meta>
-//           <Card.Description>
-//             {superpower.description}
-//           </Card.Description>
-//         </Card.Content>
-//       </Card>
-//     )}
-//   )}
-//   </div>
-// )}
   return (
   <Grid columns={3} divided>
   {props.superpowers.superpowers.map((superpower) => {
@@ -48,6 +19,12 @@ const AllSuperpowers = (props) => {
   )}
   </Grid>
 )}
+
+const mapStateToProps = (state) => {
+  return {
+    superpowers: state.superpowers
+  }
+}
 
 const AllSuperpowersContainer = withRouter(connect(mapStateToProps)(AllSuperpowers))
 
