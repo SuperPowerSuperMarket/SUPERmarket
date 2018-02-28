5d166d9b60e1fb6f70757984a4bf54a524491a45
Sunny Kim <sun-kyu.kim@aya.yale.edu>
2018-02-28 18:47:02 -0500

    more semantic ish

--- client/components/SingleSuperpower.js
+++ client/components/SingleSuperpower.js
@@ -1,38 +1,38 @@
-// import React from 'react'
-// import { Card, Icon, Image } from 'semantic-ui-react'
-// import { connect } from 'react-redux';
-// import { withRouter } from 'react-router-dom'
+import React from 'react'
+import { Card, Icon, Image } from 'semantic-ui-react'
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom'
 
-// const mapStateToProps = (state) => {
-//     return {
-//       superpowers: state.superpowers
-//     }
-//   }
+const mapStateToProps = (state) => {
+    return {
+      superpowers: state.superpowers
+    }
+  }
 
-// const SingleSuperpower = () => (
-//   <Card>
-//     <Image src='/assets/images/avatar/large/matthew.png' />
-//     <Card.Content>
-//       <Card.Header>
-//         Matthew
-//       </Card.Header>
-//       <Card.Meta>
-//         <span className='date'>
-//           Joined in 2015
-//         </span>
-//       </Card.Meta>
-//       <Card.Description>
-//         Matthew is a musician living in Nashville.
-//       </Card.Description>
-//     </Card.Content>
-//     <Card.Content extra>
-//       <a>
-//         <Icon name='user' />
-//         22 Friends
-//       </a>
-//     </Card.Content>
-//   </Card>
-// )
+const SingleSuperpower = () => (
+  <Card>
+    <Image src='/assets/images/avatar/large/matthew.png' />
+    <Card.Content>
+      <Card.Header>
+        Matthew
+      </Card.Header>
+      <Card.Meta>
+        <span className='date'>
+          Joined in 2015
+        </span>
+      </Card.Meta>
+      <Card.Description>
+        Matthew is a musician living in Nashville.
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content>
+  </Card>
+)
 
-// const SingleSuperpowerContainer = withRouter(connect(mapStateToProps))(SingleSuperpower)
-// export default SingleSuperpowerContainer;
+const SingleSuperpowerContainer = withRouter(connect(mapStateToProps))(SingleSuperpower)
+export default SingleSuperpowerContainer;
