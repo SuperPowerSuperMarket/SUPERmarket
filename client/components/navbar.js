ed7bfb4c1d61187f33277bf2449e210a02dd2523
Luis Rincon <rincon_92@hotmail.com>
2018-03-02 17:24:35 -0500

    name displays in navbar when logged in

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -17,54 +17,62 @@ import {
   Icon
 } from "semantic-ui-react";
 
-const Navbar = ({ handleClick, isLoggedIn }) => (
-  <div>
-    <Menu fixed="top" inverted>
-      <Container>
-        <Menu.Item header>
-          <Link to={'/all-superpowers'}>
-            SUPERmarket
-          </Link>
-        </Menu.Item>
-        <Menu.Item>
-          <Link to={'/home'}>
-            Home
-          </Link>
-        </Menu.Item>
-        {isLoggedIn ? (
-          <Menu.Item position="right">
-            {/* The navbar will show these links after you log in */}
-            <a href="#" onClick={handleClick}>
-              Logout
-            </a>
-          </Menu.Item>
-        ) : (
-          <Menu.Item position="right">
-            {/* The navbar will show these links before you log in */}
-            <Link to="/login">
-              <Button>Login</Button>
-            </Link>
-            <Link to="/signup">
-              <Button>Sign Up</Button>
+const Navbar = ({ handleClick, isLoggedIn, user }) => {
+  console.log(user)
+  return (
+    <div>
+      <Menu fixed="top" inverted>
+        <Container>
+          <Menu.Item header>
+            <Link to={'/all-superpowers'}>
+              SUPERmarket
             </Link>
           </Menu.Item>
-        )}
-        <Menu.Item>
-          <Link to="/cart">
-            <Icon className="shopping cart big icon" />
-          </Link>
-        </Menu.Item>
-      </Container>
-    </Menu>
-  </div>
-);
+          <Menu.Item>
+            <Link to={'/home'}>
+              Home
+            </Link>
+          </Menu.Item>
+          {isLoggedIn ? (
+            <Menu.Item position="right">
+              {/* The navbar will show these links after you log in */}
+              <h4 text style={{marginRight: '8em', marginBottom: '0.1em'}}>Hello {'' + user.firstName}</h4>
+              <a href="#" onClick={handleClick}>
+                Logout
+              </a>
+            </Menu.Item>
+          ) : (
+            <Menu.Item position="right">
+              {/* The navbar will show these links before you log in */}
+              <Link to="/login">
+                <Button>Login</Button>
+              </Link>
+              <Link to="/signup">
+                <Button>Sign Up</Button>
+              </Link>
+            </Menu.Item>
+          )}
+          <Menu.Item>
+            <Link to="/cart">
+              <Icon className="shopping cart big icon" text style={{marginRight: '1em'}}/>
+            </Link>
+            <Link to={'/home'}>
+              Account
+            </Link>
+          </Menu.Item>
+        </Container>
+      </Menu>
+    </div>
+  )
+};
 
 /**
  * CONTAINER
  */
 const mapState = state => {
   return {
-    isLoggedIn: !!state.user.id
+    isLoggedIn: !!state.user.id,
+    user: state.user
   };
 };
 
